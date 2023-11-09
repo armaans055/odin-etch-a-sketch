2763ffd74103c7a031bfbbe8eeed457ab205dba8
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-09 12:22:03 -0800

    Finished a few bug fixes, unfinished but a little left to do

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 let resetter = document.querySelector('.reset')
 for(i=0; i<1024; i++){
     let div = document.createElement("div");
+    div.className = "newdiv",
     div.style.width = "20px";
     div.style.height = "20px";
     div.style.background = "white";
     div.addEventListener("mouseover", ()=>{
         div.style.background = "black";
-    })
-    div.addEventListener("mousedown", ()=>{
-        div.style.background = "black";
+
     })
 
     document.querySelector('.container').appendChild(div)
 }
+let divs = document.querySelectorAll('.newdiv')
+
+function reset(){
+    let divs = document.querySelectorAll('.newdiv')
+    divs.style.background= "white"
+}
+
+resetter.addEventListener("click", reset())
+
