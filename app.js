ce858a822145ae64cf2b426575ed36021fc4b1fe
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-09 12:08:13 -0800

    Made etch a sketch work. Started on reset button

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-for(i=0; i<4096; i++){
+let resetter = document.querySelector('.reset')
+for(i=0; i<1024; i++){
     let div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
+    div.style.width = "20px";
+    div.style.height = "20px";
     div.style.background = "white";
+    div.addEventListener("mouseover", ()=>{
+        div.style.background = "black";
+    })
+    div.addEventListener("mousedown", ()=>{
+        div.style.background = "black";
+    })
 
     document.querySelector('.container').appendChild(div)
 }
