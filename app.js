c3c36b08e64cd4280615433120f53a2075349a9e
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-09 11:43:17 -0800

    Made layout and made all div boxes

---
+++ app.js
@@ -0,0 +1,8 @@
+for(i=0; i<4096; i++){
+    let div = document.createElement("div");
+    div.style.width = "10px";
+    div.style.height = "10px";
+    div.style.background = "white";
+
+    document.querySelector('.container').appendChild(div)
+}
